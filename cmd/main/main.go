e649734d076d20a2c28c2a11c685903943cedbf8
Eslam Abdallah <beromara128@gmail.com>
2024-04-23 22:32:57 +0200

    finish project

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -6,11 +6,12 @@ import (
 
 	"github.com/eslamabdallah74/go_book_management/pkg/routes"
 	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func main() {
 	r := mux.NewRouter()
 	routes.RegisterBookStore(r)
 	http.Handle("/", r)
-	log.Fatal(http.ListenAndServe("localhost:8000", r))
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
 }
