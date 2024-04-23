533f95984d112505c8a9f3ffda1dffbc94341c10
Eslam Abdallah <beromara128@gmail.com>
2024-04-23 21:12:13 +0200

    add cruds methods to model

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/eslamabdallah74/go_book_management/pkg/routes"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStore(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:8000", r))
 }
